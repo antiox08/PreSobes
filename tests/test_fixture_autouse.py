ad492987deeb46ecb3c707d0561ba38176dd64c2
Anton Zaika <antiox08@Mail.ru>
2026-06-29 09:45:28 +0400

    fix fixture

---
+++ tests/test_fixture_autouse.py
@@ -0,0 +1,19 @@
+import pytest
+
+db = []
+
+
+@pytest.fixture(autouse=True)
+def db_fixture():
+    db.clear()
+    yield
+    db.clear()
+
+
+def test_add_user():
+    db.append("user")
+    assert (len(db) == 1)
+
+def test_add_admin():
+    db.append("admin")
+    assert len(db) == 1
