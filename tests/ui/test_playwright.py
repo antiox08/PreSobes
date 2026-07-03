ca5196382bc1321e40b968da901bbff402e1b922
Anton Zaika <antiox08@Mail.ru>
2026-07-03 11:07:24 +0400

    Запуск Ui tests

--- tests/ui/test_playwright.py
+++ tests/ui/test_playwright.py
@@ -1,3 +1,4 @@
+import pytest
 from playwright.sync_api import Page, expect
 
 def test_wiki(page: Page):
@@ -5,11 +6,13 @@ def test_wiki(page: Page):
     page.locator('#ca-talk').get_by_role('link', name='Обсуждение').click()
     expect(page.locator('#firstHeading').get_by_text('Обсуждение' and 'Заглавная страница')).to_be_visible()
 
+@pytest.mark.ui
 def test_searh(page: Page):
     page.goto('https://ru.wikipedia.org/')
     page.locator('#searchInput').first.fill('python')
     page.get_by_role('option').first.click()
     expect(page.locator('#firstHeading')).to_have_text('Python')
+
 
 
 
