1c9094dc3ebe19046c15dd9570fe14773ca0e162
Anton Zaika <antiox08@Mail.ru>
2026-07-09 09:51:42 +0400

    Исправил замечания по неделе 4

--- conftest.py
+++ conftest.py
@@ -1,14 +1,3 @@
 import pytest
-from selenium import webdriver
 
-
-@pytest.fixture
-def driver():
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
-
-
-def test_google(driver):
-    driver.get("https://google.com")
-    assert "Google" in driver.title
+pytest_plugins = ['pytest_playwright']
