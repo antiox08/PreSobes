075ebc61a2b32297cb25eb17a71f0ee02344f724
Anton Zaika <antiox08@Mail.ru>
2026-08-28 10:12:09 +0400

    add API tests with responses and mocker.patch for week 6

---
+++ api/tests/test_get_post_mocked.py
@@ -0,0 +1,53 @@
+import pytest
+import responses
+from requests import Session
+from api.client import BASE_URL, get_post
+from pydantic import BaseModel, ConfigDict, Field
+from requests.exceptions import HTTPError
+
+
+class Post(BaseModel):
+    model_config = ConfigDict(extra="ignore")
+
+    userId: int = Field(gt=0, description="ID автора поста")
+    id: int = Field(gt=0)
+    title: str = Field(min_length=10, max_length=200)
+    body: str = Field(min_length=10)
+
+
+@pytest.mark.api
+@responses.activate
+def test_post_200_validated_with_pydantic(session: Session):
+
+    fake_post = {
+        "userId": 1,
+        "id": 1,
+        "title": "Это заголовок статьи",
+        "body": "Это тело статьи",
+    }
+
+    responses.add(responses.GET, url=f"{BASE_URL}/posts/1", json=fake_post, status=200)
+
+    result = get_post(session, 1)
+    post = Post.model_validate(result)
+
+    assert post.id == 1
+    assert post.title == "Это заголовок статьи"
+
+
+@pytest.mark.api
+@responses.activate
+def test_post_404_invalid(session):
+    responses.add(responses.GET, url=f"{BASE_URL}/posts/1", status=404)
+
+    with pytest.raises(HTTPError):
+        get_post(session, 1)
+
+
+@pytest.mark.api
+@responses.activate
+def test_post_500_invalid(session):
+    responses.add(responses.GET, url=f"{BASE_URL}/posts/1", status=500)
+
+    with pytest.raises(HTTPError):
+        get_post(session, 1)
---
+++ api/tests/test_get_post_mocker_path.py
@@ -0,0 +1,36 @@
+import pytest
+from unittest.mock import Mock
+from requests import Session
+from api.client import get_post
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class Post(BaseModel):
+    model_config = ConfigDict(extra="ignore")
+
+    userId: int = Field(gt=0, description="ID автора поста")
+    id: int = Field(gt=0)
+    title: str = Field(min_length=10, max_length=200)
+    body: str = Field(min_length=10)
+
+
+@pytest.mark.api
+def test_get_200_with_mocker(session: Session, mocker):
+
+    mock_response = Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {
+        "userId": 1,
+        "id": 1,
+        "title": "Это заголовок статьи - 2",
+        "body": "Это тело статьи - 2",
+    }
+
+    mock_response.raise_for_status.return_value = None
+
+    mocker.patch.object(session, "get", return_value=mock_response)
+
+    result = get_post(session, 1)
+
+    post = Post.model_validate(result)
+    assert post.id == 1
